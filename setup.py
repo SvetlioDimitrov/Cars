c24b3a5a6db79ddc5692ab0c7197e2e0e5cb1970
SvetlioDimitrov <svetlio.dimitrov@hotmail.com>
2023-09-20 13:15:23 +0300

    Upload files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='AdastraTask',
+    version='1.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Svetlio',
+    author_email='',
+    description='Cars'
+)
